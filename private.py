b675b36b949b2feb96aab434da6cd3bad1114f46
Dereek69 <31802769+Dereek69@users.noreply.github.com>
2023-08-19 21:56:35 +0200

    Update private.py

--- private.py
+++ private.py
@@ -1,3 +1,3 @@
 private_key = "YOUR PRIVATE KEY HERE"
 telegram_bot_token = "YOUR TELEGRAM BOT TOKEN HERE"
-telegram_chat_id = "YOUR TELEGRAM CHAT ID HERE
+telegram_chat_id = "YOUR TELEGRAM CHAT ID HERE"
